5d58d3023eb9421cb81c04b8cfe62b8691c792b4
Arseniy Khvorov <khvorov45@gmail.com>
2023-08-03 12:18:15 +1000

    change colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
